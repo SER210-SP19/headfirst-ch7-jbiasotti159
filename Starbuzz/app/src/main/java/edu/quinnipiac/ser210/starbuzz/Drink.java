95a0fbc926c4b64edbca753258d9ed16fcaa766e
jillianbiasotti <jnbiasotti@quinnipiac.edu>
2019-02-26 00:50:32 -0500

    Chapter 7 Demo

---
+++ Starbuzz/app/src/main/java/edu/quinnipiac/ser210/starbuzz/Drink.java
@@ -0,0 +1,42 @@
+package edu.quinnipiac.ser210.starbuzz;
+
+import android.widget.Adapter;
+
+public class Drink{
+
+    private String name;
+    private String description;
+    private int imageResourceID;
+
+
+    public static final Drink[] drinks = {
+            new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
+            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
+    };
+
+    private Drink(String name, String description, int imageResourceID)
+    {
+        this.name = name;
+        this.description = description;
+        this.imageResourceID = imageResourceID;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public int getImageResourceID()
+    {
+        return imageResourceID;
+    }
+    public String toString()
+    {
+        return this.name;
+    }
+}
